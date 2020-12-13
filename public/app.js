29f667fb89375e17bea54918799a6b9cd2415091
S.Kiri <s.kirianov@gmail.com>
2020-12-13 21:33:28 +0100

    trying firebase auth

--- public/app.js
+++ public/app.js
@@ -12,8 +12,22 @@ const theHobbit = new Book('The hobbit','Tolkien',295,'not read yet');
 
 console.log(theHobbit.info());
 
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
-
+ui.start('#firebaseui-auth-container', {
+    signInOptions: [
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+  });
+ui.start('#firebaseui-auth-container', {
+    signInOptions: [
+      {
+        provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD
+      }
+    ],
+    // Other config options...
+  });
 
 
 
@@ -27,8 +41,8 @@ function onSuccess(googleUser) {
   function renderButton() {
     gapi.signin2.render('my-signin2', {
       'scope': 'profile email',
-      'width': 240,
-      'height': 50,
+      'width': 1000,
+      'height': 220,
       'longtitle': true,
       'theme': 'dark',
       'onsuccess': onSuccess,
