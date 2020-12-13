af336a50634d09d3ed3d3348fa62beb4aa3e1f39
S.Kiri <s.kirianov@gmail.com>
2020-12-13 19:27:11 +0100

    Completed visuals login page

--- public/app.js
+++ public/app.js
@@ -11,3 +11,27 @@ const Book = function(title, author, pages, read){
 const theHobbit = new Book('The hobbit','Tolkien',295,'not read yet');
 
 console.log(theHobbit.info());
+
+
+
+
+
+
+
+function onSuccess(googleUser) {
+    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
+  }
+  function onFailure(error) {
+    console.log(error);
+  }
+  function renderButton() {
+    gapi.signin2.render('my-signin2', {
+      'scope': 'profile email',
+      'width': 240,
+      'height': 50,
+      'longtitle': true,
+      'theme': 'dark',
+      'onsuccess': onSuccess,
+      'onfailure': onFailure
+    });
+  }
