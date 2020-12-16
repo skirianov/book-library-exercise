34e65f7f5750729d7497861071bdabb0755ce3db
S.Kiri <s.kirianov@gmail.com>
2020-12-16 22:09:36 +0100

    rectified auth

--- public/auth.js
+++ public/auth.js
@@ -1,23 +1,31 @@
-const formEmailPassword = document.querySelector('#signin-form');
+const signInEmailPassword = document.querySelector('#signin-form');
+const loginForm = document.querySelector('#login-form');
 
-formEmailPassword.addEventListener('submit',(e) => {
+
+signInEmailPassword.addEventListener('submit',(e) => {
     e.preventDefault();
 
-    const email = formEmailPassword["email"].value;
-    const password = formEmailPassword["password"].value;
+    const name = signInEmailPassword["name"].value;
+    const password = signInEmailPassword["password"].value;
+    const email = signInEmailPassword["email"].value;
 
     auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        console.log(cred);
     });
 
-    formEmailPassword["email"].value = '';
-    formEmailPassword["password"].value = '';
+        signInEmailPassword["email"].value = '';
+        signInEmailPassword["password"].value = '';
+        signInEmailPassword["name"].value = '';
+        hideSignInModal();
 });
 
 const login = document.querySelector('#login');
 
-login.addEventListener('click', () => {
-    const email = formEmailPassword["email"].value;
-    const password = formEmailPassword["password"].value;
+login.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+    const email = loginForm["email"].value;
+    const password = loginForm["password"].value;
 
     modal.style.display = "block";
     page.style.filter = "blur(2px)";
@@ -25,18 +33,12 @@ login.addEventListener('click', () => {
 
     firebase.auth().signInWithEmailAndPassword(email, password)
   .then((user) => {
-        if (user.additionalUserInfo.isNewUser === false){
-            loggedInMainPage();
-        } else {
-            newUserRegRequest();
-        }
-
+    loggedInMainPage();
+    console.log(user);
   })
   .catch((error) => {
     var errorCode = error.code;
     var errorMessage = error.message;
-
-    console.log(errorMessage, errorCode)
   });
 })
 
